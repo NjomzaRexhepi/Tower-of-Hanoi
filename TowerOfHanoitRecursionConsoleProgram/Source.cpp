661dd9232f71dbe325a24b0c37cb075b32929baa
Njomza Rexhepi <75095687+NjomzaRexhepi@users.noreply.github.com>
2023-10-03 16:36:22 +0200

    Update Source.cpp

--- TowerOfHanoitRecursionConsoleProgram/Source.cpp
+++ TowerOfHanoitRecursionConsoleProgram/Source.cpp
@@ -1,20 +1,6 @@
 #include<iostream>
 using namespace std;
-int binarySearch(int arr[], int toBeSearched, int first, int last) {
-	if (first > last) {
-		return -1;
-	}
-	int mid = (first + last) / 2;
-	if (arr[mid] == toBeSearched) {
-		return mid;
-	}
-	else if (arr[mid] > toBeSearched) {
-		return binarySearch(arr, toBeSearched, first, mid - 1);
-	}
-	else {
-		return binarySearch(arr, toBeSearched, mid + 1, last);
-	}
-}
+
 
 void towersOfHanoi(int start, int end, char source, char aux, char destination) {
 	if (start > end) {
